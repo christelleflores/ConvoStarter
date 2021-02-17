5b1d86519d22b9034ef74307930dfefc8f9ac20f
christelle flores <57645973+christelleflores@users.noreply.github.com>
2021-02-17 17:02:56 -0500

    fix case sensitive response

--- src/Test.java
+++ src/Test.java
@@ -7,16 +7,17 @@ public class Test {
 
 		System.out.println("Hello! Would you like to start a conversation? Yes or No");
 		String response = scanner.nextLine();
-		if (response.equals("No")) {
+		if (response.toLowerCase().equals("no")) {
 			System.out.println("Okay... bye I guess.");
-		} else if (response.equals("Yes")) {
+		} else if (response.toLowerCase().equals("yes")) {
 			System.out.println("Great! What would you like to talk about?");
 
 			String[] convos = new String[3];
 			convos[0] = "School";
 			convos[1] = "Animals";
 			convos[2] = "Cars";
-
+			
+			String response2 = scanner.nextLine();
 		}
 
 	}
